55bc71a18fba8accef1f98d964f6a95e397bf34a
chirag malik <chiragmalik2612@gmail.com>
2025-07-18 15:21:40 +0530

    Update HeroSection.jsx

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -13,20 +13,11 @@ const HeroSection = () => {
       <div className={styles.textContainer}>
         <h1 className={styles.heading}>Hi, I'm Chirag Malik</h1>
         <p className={styles.subheading} style={{ fontStyle: "italic" }}>
-          IIT BHU | Web Developer | Research Intern @IITDelhi | SME @PW | JEE
+          IIT BHU | Web Developer | SME @PW | ex-Research Intern @IITDelhi  | ex-JEE
           Mentor @PW
         </p>
         <p className={styles.description}>
-          I’m a full-stack web developer and undergraduate student at IIT (BHU),
-          Varanasi. I build web applications with a focus on clean UI,
-          performance, and usability. I’ve worked for nearly a year as a JEE
-          Mentor at Physics Wallah and currently contribute as a Subject Matter
-          Expert in Physics. Alongside this, I’m also a Research Intern at IIT
-          Delhi. I’m actively building projects, improving my technical skills,
-          and applying them in both education and research contexts. I’m always
-          up for creating useful and practical solutions using my skills, and
-          I’m open to connecting with like-minded people for collaboration and
-          learning.
+          I’m a full-stack web developer and undergraduate student at IIT (BHU), Varanasi. I build web applications with a focus on clean UI, performance, and usability. I’ve worked for nearly a year as a JEE Mentor at Physics Wallah and currently contribute as a Subject Matter Expert in Physics. I previously worked as a Research Intern at IIT Delhi, where I applied my skills in a research-driven environment and gained valuable hands-on experience. I’m constantly building new projects, sharpening my technical abilities, and applying them across both education and technology. I'm always excited to collaborate, create meaningful solutions, and connect with like-minded individuals.
         </p>
         {/* Action Buttons */}
         <div className={styles.buttonGroup}>
