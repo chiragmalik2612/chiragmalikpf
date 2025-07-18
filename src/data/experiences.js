7d6655d7cbb87625c47a4ce5527a4c612893da45
chirag malik <chiragmalik2612@gmail.com>
2025-07-18 15:25:54 +0530

    Update experiences.js

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,15 +1,6 @@
 
 export const experiences = [
   {
-    title: "Research Intern",
-    slug: "research-intern",
-    org: "IIT Delhi",
-    date: "May 2025 – Present",
-    type: "Intern",
-    description:
-      "Working on microfluidics and Lab-on-a-Chip systems in a biochemical research lab, focusing on fluid behavior at micro-scale and device development.",
-  },
-  {
     title: "SME - Physics",
     slug: "sme-physics",
     org: "Physics Wallah",
@@ -17,6 +8,15 @@ export const experiences = [
     type: "Content",
     description:
       "Working on JEE Physics doubt resolution and preparing accurate, high-quality solutions for student queries.",
+  },
+   {
+    title: "Research Intern",
+    slug: "research-intern",
+    org: "IIT Delhi",
+    date: "May 2025 – July 2025",
+    type: "Intern",
+    description:
+      "Gained hands-on experience with microfluidic and Lab-on-a-Chip systems, exploring microscale fluid flow and assisting in device development in a biochemical research setting.",
   },
   {
     title: "JEE Mentor",
